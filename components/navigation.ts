50c580d184acd110e8b01b5ce54df870367918ef
Jake741633 <jakerinaldi1308@hotmail.com>
2026-07-29 20:37:20 +0100

    Add certificates navigation

--- components/navigation.ts
+++ components/navigation.ts
@@ -10,5 +10,5 @@ export const primaryNavigation = [
 
 export const secondaryNavigation = [
   ["Quotes", "/quotes"], ["Estimates", "/estimates"], ["Invoices", "/invoices"], ["Materials", "/materials"],
-  ["Surveys", "/surveys"], ["Job Packs", "/job-packs"], ["JR AI", "/ai"], ["Business", "/business"],
+  ["Surveys", "/surveys"], ["Certificates", "/certificates"], ["Job Packs", "/job-packs"], ["JR AI", "/ai"], ["Business", "/business"],
 ] as const;
