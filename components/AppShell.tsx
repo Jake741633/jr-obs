f7f42e23b412c3c48b519371469be03ed01af2bf
Jake741633 <jakerinaldi1308@hotmail.com>
2026-07-30 22:08:09 +0100

    Show global cloud sync state

--- components/AppShell.tsx
+++ components/AppShell.tsx
@@ -1,8 +1,9 @@
 import type { ReactNode } from "react";
 import { CloudAccessGuard } from "./CloudAccessGuard";
+import { CloudSyncIndicator } from "./CloudSyncIndicator";
 import Sidebar from "./Sidebar";
 import { MobileNav } from "./MobileNav";
 
 export function AppShell({ children }: { children: ReactNode }) {
-  return <div className="min-h-screen bg-slate-950 text-white"><Sidebar /><main className="min-h-screen pb-24 lg:ml-72 lg:pb-0"><div className="mx-auto w-full max-w-7xl px-4 py-6 sm:px-6 lg:px-8 lg:py-8"><CloudAccessGuard>{children}</CloudAccessGuard></div></main><MobileNav /></div>;
+  return <div className="min-h-screen bg-slate-950 text-white"><Sidebar /><CloudSyncIndicator /><main className="min-h-screen pb-24 lg:ml-72 lg:pb-0"><div className="mx-auto w-full max-w-7xl px-4 py-6 sm:px-6 lg:px-8 lg:py-8"><CloudAccessGuard>{children}</CloudAccessGuard></div></main><MobileNav /></div>;
 }
