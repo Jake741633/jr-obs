f14b9d28bf65a97c6ac1829fc319293b091163f8
Jake741633 <jakerinaldi1308@hotmail.com>
2026-07-31 09:00:42 +0100

    Add jobs cloud collection hook

--- lib/cloud/coreBusinessCollections.ts
+++ lib/cloud/coreBusinessCollections.ts
@@ -1,12 +1,17 @@
 "use client";
 
-import type { Customer } from "../models";
+import type { Customer, Job } from "../models";
 import { useCloudLocalCollection } from "../storage";
 
 export const coreBusinessStorageKeys = {
   customers: "jr-os-customers",
+  jobs: "jr-os-jobs",
 } as const;
 
 export function useCustomersCollection() {
   return useCloudLocalCollection<Customer>(coreBusinessStorageKeys.customers);
+}
+
+export function useJobsCollection() {
+  return useCloudLocalCollection<Job>(coreBusinessStorageKeys.jobs);
 }
