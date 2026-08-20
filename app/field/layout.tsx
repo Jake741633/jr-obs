4f744e5cf4af2415fd70449095304551b205163b
Jake741633 <jakerinaldi1308@hotmail.com>
2026-08-20 18:56:05 +0100

    Show field workspace navigation

--- app/field/layout.tsx
+++ app/field/layout.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from "react";
+import { FieldWorkspaceNav } from "../../components/FieldWorkspaceNav";
 import { ScheduleOverview } from "../../components/ScheduleOverview";
 
 export default function FieldLayout({ children }: { children: ReactNode }) {
-  return <div className="space-y-6"><ScheduleOverview mobile />{children}</div>;
+  return <div className="space-y-6"><ScheduleOverview mobile /><FieldWorkspaceNav />{children}</div>;
 }
